7b1086b8fae882643eedac5b8091427ad2e9109c
Rudra-M1 <rudraprajapati640@gmail.com>
2026-02-03 12:57:31 +0530

    firebase env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeJjbvHTxkBmelmA7-5flu8kV455nUNd4",
-  authDomain: "craftixglobal.firebaseapp.com",
-  projectId: "craftixglobal",
-  storageBucket: "craftixglobal.appspot.com",   // ✅ IMPORTANT
-  messagingSenderId: "1092743193153",
-  appId: "1:1092743193153:web:13823cccfdf364337a9cc0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
